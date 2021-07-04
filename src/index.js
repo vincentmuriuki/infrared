16182ec4bd22e1b27f6a1f73ecdf4bc8b27464ed
vincentmuriuki <vincentmuriuki42@gmail.com>
2021-07-04 16:22:07 +0300

    Add dashboard static page

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import AuthLayout from "layouts/Auth/Auth.js";
+import AdminLayout from "layouts/Admin/Admin.js";
+import RTLLayout from "layouts/RTL/RTL.js";
+
+import "assets/css/nucleo-icons.css";
+import "react-notification-alert/dist/animate.css";
+import "assets/scss/black-dashboard-pro-react.scss?v=1.1.0";
+import "assets/demo/demo.css";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/auth" render={props => <AuthLayout {...props} />} />
+      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      <Route path="/rtl" render={props => <RTLLayout {...props} />} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
